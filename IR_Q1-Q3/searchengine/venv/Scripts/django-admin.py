20937041a018612523cbbf5633c55a889cfbd715
joeypce <31504329+joeypce@users.noreply.github.com>
2020-01-30 16:37:27 +0800

    .

---
+++ IR_Q1-Q3/searchengine/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Joey\Desktop\PycharmProjects\IR_SearchEngine\IR_Q1-Q3\searchengine\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
